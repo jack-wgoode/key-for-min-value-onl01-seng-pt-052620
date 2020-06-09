52b0596e6b682ad58cec0acbcac109cc7f61340f
Jack Goode <jack.w.goode@icloud.com>
2020-06-09 18:28:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-    min_value =  
+    min_key = nil
+    min_value = nil 
     name_hash.each do |key, value|
       if value < value
         min_value = value
