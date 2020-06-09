944969b16d0ebbec520723a2917325a83b47f3d5
Jack Goode <jack.w.goode@icloud.com>
2020-06-09 18:35:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
     
     name_hash.each do |key, value|
       if min_value == nil || value < min_value
-      if value < value
-        min_value = value
+         min_value = value
+         min_key = key
       end
       key
 end
